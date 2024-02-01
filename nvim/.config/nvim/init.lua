5dc384d585093c7f78fcf026629da0710944c3fd
s-k-zaman <sudo.coder.zaman@gmail.com>
2024-02-02 02:14:35 +0530

    nvim: nightfox colorscheme added

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- FIX: c-i is behaving strange, no mapping is Working
 -- COLORSCHEME = "nordfox"
+COLORSCHEME = "nightfox"
 -- COLORSCHEME = "onedark"
-COLORSCHEME = "rose-pine"
+-- COLORSCHEME = "rose-pine"
 Disable_Lsp_Server_Formatting = true
 require("config")
--- nvim/.config/nvim/lua/plugins/colorschems.lua
+++ nvim/.config/nvim/lua/plugins/colorschems.lua
@@ -1,5 +1,5 @@
 return {
     { "navarasu/onedark.nvim", enabled = true, lazy = false, priority = 1000 },
     { "EdenEast/nightfox.nvim", lazy = false, priority = 1000 },
-    { "rose-pine/neovim", name = "rose-pine", priority = 1000 },
+    { "rose-pine/neovim", name = "rose-pine", lazy = false, priority = 1000 },
 }
